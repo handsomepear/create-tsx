9e1d0be7be9629d733452a94393587600c107910
翟培伸 <zhaipeishen@lixiang.com>
2023-04-25 17:52:07 +0800

    feat: V0.0.1 scripts

---
+++ README.md
@@ -0,0 +1,17 @@
+# 使用命令行创建 tsx 文件并写入默认模板
+
+## 使用方法
+
+> 还未发布到 npm，可以 clone 代码之后执行 `npm link` 创建软连接到本地
+
+- 创建目录类型的组件 例如：/components/Auth/index.tsx
+
+```
+ctsx /components/Auth
+```
+
+- 创建 .tsx 文件
+
+```
+ctsx /components/Auth/Auth.tsx
+```
---
+++ index.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const willWritePath = process.argv[2];
+
+const isCreateFolder = !willWritePath.endsWith('.tsx');
+
+const write = () => {
+  fs.readFile(path.join(__dirname, './tsx.template'), (err, data) => {
+    if (err) throw err;
+
+    const filename = willWritePath.split('/').at(-1).replace('.tsx', '');
+
+    const fileContet = data.toString().replace(/\$\$File/g, filename);
+
+    const filePath = path.join(
+      process.cwd(),
+      isCreateFolder ? `${willWritePath}/index.tsx` : willWritePath
+    );
+
+    if (isCreateFolder) {
+      createFolder(path.join(process.cwd(), willWritePath));
+    }
+
+    fs.writeFile(filePath, fileContet, (err) => {
+      if (err) throw err;
+      console.log(`✅ ${filePath} created successfully！`);
+    });
+  });
+};
+
+const createFolder = (folderPath) => {
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+};
+
+write();
